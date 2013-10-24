d66d65879fddf432e745bbda319af7a4b6e04080
guoku engineer <gkeng@workstation.(none)>
2013-10-24 19:46:44 +0800

    update

--- crawler/listener.sh
+++ crawler/listener.sh
@@ -2,13 +2,20 @@
 pgrep -f "node ./post_server.js"
 if [ $? != 0 ]
 then
-    cd /home/zlu/code/Grape/crawler/
+    cd /home/gkeng/Grape/crawler/
     nohup /usr/local/bin/node ./post_server.js 1>log_post.log 2>log_post_error.log &
 fi
 
 pgrep -f "node ./get_server.js"
 if [ $? != 0 ]
 then
-    cd /home/zlu/code/Grape/crawler/
+    cd /home/gkeng/Grape/crawler/
     nohup /usr/local/bin/node ./get_server.js 1>log_get.log 2>log_get_error.log &
+fi
+
+pgrep -f "node ./image_server.js"
+if [ $? != 0 ]
+then
+    cd /home/gkeng/Grape/crawler/
+    nohup /usr/local/bin/node ./image_server.js 1>log_image.log 2>log_image_error.log &
 fi
